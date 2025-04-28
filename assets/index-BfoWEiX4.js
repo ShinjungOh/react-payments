7005248eb67f5d1003feb71a5d827b66fc05dfd7
ShinjungOh <tlswjd9260@gmail.com>
2025-04-28 16:54:29 +0900

    Updates

--- assets/index-BfoWEiX4.js
+++ assets/index-BfoWEiX4.js
@@ -17006,7 +17006,7 @@ function CardRegistrationCompletePage() {
     return /* @__PURE__ */ jsx$1(Navigate, { to: "/", replace: true });
   }
   return /* @__PURE__ */ jsxs("div", { css: pageContainer, children: [
-    /* @__PURE__ */ jsx$1("img", { src: Complete, css: completeImage }),
+    /* @__PURE__ */ jsx$1("img", { src: Complete, alt: "완료", css: completeImage }),
     /* @__PURE__ */ jsxs("h1", { css: title, children: [
       cardNumber.first,
       "로 시작하는",
